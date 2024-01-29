9b2a4638f91afe6738c8f8da34b2ecb20a217c8d
jaswa ramesh <jaswant.r@kalvium.community>
2024-01-29 11:19:32 +0530

    commit

---
+++ hooks/src/components/InputField.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import Storage from './Storage'
+
+const InputField = () => {
+
+    let [value, setValue] = useState("")
+    let [storedValue, setStoredValue] = Storage("Key")
+
+    function inputData (e){
+        let data = e.target.value;
+        setValue(data)
+        setStoredValue(data)
+    }
+      
+  return (
+    <>
+        <input type="text"
+        placeholder = "OH hii.."
+        onChange = {inputData}
+        className="input-field"
+        />
+        <p>This is the Data: {value}</p>
+    </>
+  )
+}
+
+export default InputField;
